63e647ca6d85279d812091ae0cdcb00a0fd3e502
Siyuan Gao <siyuangao@gmail.com>
2015-09-05 10:44:39 -0700

    Initial commit

---
+++ gluster/bin/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+[ "$DEBUG" == "1" ] && set -x && set +e
+
+if [ "${GLUSTER_PEER}" == "**ChangeMe**" ]; then
+   echo "ERROR: You did not specify "GLUSTER_PEER" environment variable - Exiting..."
+   exit 0
+fi
+
+ALIVE=0
+for PEER in `echo "${GLUSTER_PEER}" | sed "s/,/ /g"`; do
+    echo "=> Checking if I can reach GlusterFS node ${PEER} ..."
+    if ping -c 10 ${PEER} >/dev/null 2>&1; then
+       echo "=> GlusterFS node ${PEER} is alive"
+       ALIVE=1
+       break
+    else
+       echo "*** Could not reach server ${PEER} ..."
+    fi
+done
+
+if [ "$ALIVE" == 0 ]; then
+   echo "ERROR: could not contact any GlusterFS node from this list: ${GLUSTER_PEER} - Exiting..."
+   exit 1
+fi
+
+echo "=> Mounting GlusterFS volume ${GLUSTER_VOL} from GlusterFS node ${PEER} ..."
+mount -t glusterfs ${PEER}:/${GLUSTER_VOL} ${GLUSTER_VOL_PATH}
